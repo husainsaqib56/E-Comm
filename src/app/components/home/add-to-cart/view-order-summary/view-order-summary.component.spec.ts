881d4b2b5557ab2078296e6da3d49d1afa9421e7
husainsaqib56 <husainsaqib56@gmail.com>
2021-01-14 16:22:39 +0530

    updated-changes

--- src/app/components/home/add-to-cart/view-order-summary/view-order-summary.component.spec.ts
+++ src/app/components/home/add-to-cart/view-order-summary/view-order-summary.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ViewOrderSummaryComponent } from './view-order-summary.component';
 
@@ -6,7 +6,7 @@ describe('ViewOrderSummaryComponent', () => {
   let component: ViewOrderSummaryComponent;
   let fixture: ComponentFixture<ViewOrderSummaryComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ViewOrderSummaryComponent ]
     })
