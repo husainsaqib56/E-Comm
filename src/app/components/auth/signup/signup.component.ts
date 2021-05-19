e97cf893069540ef4e422046e0e2243abbe4c04f
husainsaqib56 <husainsaqib56@gmail.com>
2021-05-19 21:54:47 +0530

    some-changes

--- src/app/components/auth/signup/signup.component.ts
+++ src/app/components/auth/signup/signup.component.ts
@@ -4,28 +4,28 @@ import { ApiService } from 'src/app/services/api.service';
 import { ISignupModel } from '../../models/productList.model';
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+	selector: 'app-signup',
+	templateUrl: './signup.component.html',
+	styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
 
-  constructor(private apiService: ApiService, private toastr: ToastrService) { }
-  username: string;
-  password: string;
-  signUpResponse: ISignupModel;
+	constructor(private apiService: ApiService, private toastr: ToastrService) { }
+	username: string;
+	password: string;
+	signUpResponse: ISignupModel;
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
-  signUp(): void {
-    this.apiService.signup(this.username, this.password).subscribe((res: ISignupModel) => {
-      this.signUpResponse = res;
-      this.toastr.success('Sucessfully', 'Signup');
-      console.log(this.signUpResponse);
-      this.username = '';
-      this.password = '';
-    });
-  }
+	signUp(): void {
+		this.apiService.signup(this.username, this.password).subscribe((res: ISignupModel) => {
+			this.signUpResponse = res;
+			this.toastr.success('Sucessfully', 'Signup');
+			console.log(this.signUpResponse);
+			this.username = '';
+			this.password = '';
+		});
+	}
 
 }
